1dee8a7b0c693923b262262fb49617db4c152946
jdain1 <24dd1301@sookmyung.ac.kr>
2026-05-24 22:35:13 +0900

    구현 완성

--- FEweek04_hw/src/components/card.jsx
+++ FEweek04_hw/src/components/card.jsx
@@ -1,12 +1,51 @@
-const Card = ({ name, category, desc, image, isRecommended }) => {
+import styled from 'styled-components';
+import Label from './Label';
+
+const CardBox = styled.div`
+    width: 507px;
+    height: 532px;
+    border-radius: 20px;
+    border: 2px dashed #878787;
+    padding: 20px;
+    text-align: center;
+    position: relative;
+`;
+
+const CardImg = styled.img`
+    width: 293px;
+    height: 332px;
+    object-fit: cover;
+    border-radius: 12px;
+    margin: 10px 0;
+`;
+
+const CardName = styled.h3`
+    color: #000;
+    text-align: center;
+    font-size: 48px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 60px; /* 125% */
+`;
+
+const CardDesc = styled.p`
+    color: #000;
+    font-size: 27px;
+    font-weight: 400;
+    line-height: 40px;
+    white-space: pre-line;   /* \n 줄바꿈 적용 */
+`;
+
+const Card = ({ name, category, desc, image }) => {
   return (
     <CardBox>
-      {isRecommended ? <Badge>Best</Badge> : null}
-      <Label category={category} />
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p>{desc}</p>
+        {/* 삼항연산자: category가 양식이면 "양식", 아니면 "한식" */}
+        <Label category={category === '양식' ? '양식' : '한식'} />
+        <CardImg src={image} alt={name} />
+        <CardName>{name}</CardName>
+        <CardDesc>{desc}</CardDesc>
     </CardBox>
   );
 };
+
 export default Card;
