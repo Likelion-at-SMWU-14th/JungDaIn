b91d5775fd7393ceb6525707f2f49ce773229751
jdain1 <24dd1301@sookmyung.ac.kr>
2026-06-30 17:04:54 +0900

    style: Movie.jsx 스타일 설정

--- FEweek06/src/App.jsx
+++ FEweek06/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import UserInfo from './components/Info';
-import DebouncedSearch from './components/DebouncedSearch';
+import Movie from "./components/Movie"
 
 function App() {
   return (
     <div>
-      <UserInfo />
-      <DebouncedSearch />
+      <Movie />
     </div>
   );
 }
