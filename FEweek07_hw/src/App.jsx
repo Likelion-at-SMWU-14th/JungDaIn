95917129a72931fb200205c4261301c09cd4fc41
jdain1 <24dd1301@sookmyung.ac.kr>
2026-07-12 16:58:01 +0900

    feat: App.jsx 작성

--- FEweek07_hw/src/App.jsx
+++ FEweek07_hw/src/App.jsx
@@ -1,7 +1,28 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Favorit from "./pages/Favorit";
+import { useFavorites } from "./hooks/useFavorites";
+import { songs } from "./data/songs";
 
 function App() {
-  return <></>;
+  const { favoriteIds, toggleFavorite, isFavorite } = useFavorites();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Main songs={songs} isFavorite={isFavorite} toggleFavorite={toggleFavorite} />
+          }
+        />
+        <Route
+          path="/favorit"
+          element={<Favorit songs={songs} favoriteIds={favoriteIds} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
