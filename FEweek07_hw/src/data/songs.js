5be6c619d04c5466f87f9ddce8f41090e4d28558
jdain1 <24dd1301@sookmyung.ac.kr>
2026-07-12 13:48:55 +0900

    Feat: 폴더 구조 생성 및 곡 더미데이터 세팅

---
+++ FEweek07_hw/src/data/songs.js
@@ -0,0 +1,67 @@
+import music1 from "../assets/music1.jpg";
+import music2 from "../assets/music2.jpg";
+import music3 from "../assets/music3.jpg";
+import music4 from "../assets/music4.jpg";
+import music5 from "../assets/music5.jpg";
+import music6 from "../assets/music6.jpg";
+
+export const songs = [
+  {
+    id: 1,
+    title: "갑자기",
+    artist: "아이오아이 (I.O.I)",
+    cover: music1,
+    album: "I.O.I 3rd MINI ALBUM [I.O.I : LOOP]",
+    releaseDate: "2026.05.19",
+    genre: "댄스",
+    flac: "Flac 16/24bit",
+  },
+  {
+    id: 2,
+    title: "REDRED",
+    artist: "CORTIS (코르티스)",
+    cover: music2,
+    album: "GREENGREEN",
+    releaseDate: "2026.04.20",
+    genre: "댄스",
+    flac: "Flac 16bit",
+  },
+  {
+    id: 3,
+    title: "LOVE ATTACK",
+    artist: "RESCENE (리센느)",
+    cover: music3,
+    album: "SCENEDROME",
+    releaseDate: "2024.08.27",
+    genre: "댄스",
+    flac: "Flac 16/24bit",
+  },
+  {
+    id: 4,
+    title: "It's Me",
+    artist: "아일릿 (ILLIT)",
+    cover: music4,
+    album: "MAMIHLAPINATAPAI",
+    releaseDate: "2026.04.30",
+    genre: "댄스",
+    flac: "Flac 16/24bit",
+  },
+  { id: 5, 
+    title: "LEMONADE", 
+    artist: "aespa", 
+    cover: music5, 
+    album: "LEMONADE - The 2nd Album", 
+    releaseDate: "2026.05.29", 
+    genre: "일렉트로니카", 
+    flac: "Flac 16/24bit" 
+  },
+  { id: 6, 
+    title: "Pretty Girl", 
+    artist: "RESCENE (리센느)", 
+    cover: music6, 
+    album: "Pretty Girl - Special Single", 
+    releaseDate: "2026.07.08", 
+    genre: "댄스", 
+    flac: "Flac 16/24bit" 
+  },
+];
