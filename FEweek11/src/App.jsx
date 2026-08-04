a1162732c674be5d81c70aff95ffb6f8ebc4a0b4
jdain1 <24dd1301@sookmyung.ac.kr>
2026-08-04 15:50:41 +0900

    feat: TitleContext 적용

--- FEweek11/src/App.jsx
+++ FEweek11/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import TitleContext from "./context/TitleContext";
 
 function App() {
   return (
-    <>
+    <TitleContext.Provider value={"dain의 영화관"}>
       <Header />
       <Outlet />
-    </>
+    </TitleContext.Provider>
   );
 }
 
