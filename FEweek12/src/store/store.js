4066e1bfe9f4b8f495d6ae799982ec1b0d4c76df
jdain1 <24dd1301@sookmyung.ac.kr>
2026-08-11 16:18:22 +0900

    feat: perist middleware 적용

--- FEweek12/src/store/store.js
+++ FEweek12/src/store/store.js
@@ -1,36 +1,41 @@
 import { create } from 'zustand';
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 const useTodoStore = create(
-    devtools((set) => ({
-    todos: [],
-    addTodo: (rawTitle) => {
-        const title = rawTitle.trim();
-        if (!title) return;
-        set((state) => ({
-            todos: [...state.todos, { id: Date.now(), title, completed: false }],
+    devtools(
+        persist(
+            (set) => ({
+        todos: [],
+        addTodo: (rawTitle) => {
+            const title = rawTitle.trim();
+            if (!title) return;
+            set((state) => ({
+                todos: [...state.todos, { id: Date.now(), title, completed: false }],
+            }),
+            undefined,
+            "todo/addTodo",
+            );
+        },
+        removeTodo: (id) =>
+            set((state) => ({
+                todos: state.todos.filter((todo) => todo.id !== id),
+            }),
+            undefined,
+            "todo/removeTodo",
+            ),
+        toggleTodo: (id) =>
+            set((state) => ({
+                todos: state.todos.map((todo) =>
+                    todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+            ),
         }),
         undefined,
-        "todo/addTodo",
-        );
-    },
-    removeTodo: (id) =>
-        set((state) => ({
-            todos: state.todos.filter((todo) => todo.id !== id),
+        "todo/toggleTodo",
+        ),
         }),
-        undefined,
-        "todo/removeTodo",
-        ),
-    toggleTodo: (id) =>
-        set((state) => ({
-            todos: state.todos.map((todo) =>
-                todo.id === id ? { ...todo, completed: !todo.completed } : todo,
-        ),
-    }),
-    undefined,
-    "todo/toggleTodo",
+        { name: "todo-store" },
     ),
-    })),
+    ),
 );
 
 export default useTodoStore;
